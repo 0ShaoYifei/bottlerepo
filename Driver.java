ff8bcd9b7974796adb80441b8ec46013fad97569
ShaoYifei <147827251+0ShaoYifei@users.noreply.github.com>
2023-10-16 16:35:27 +0800

    1
    Co-Authored-By: Doug Rudolph <6809522+11@users.noreply.github.com>

---
+++ Driver.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Driver {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        bottle bottle1=new bottle();
+        System.out.println("Bottle App V 1.0");
+        bottle1.setDetails();
+        bottle1.print();
+        
+    }
+}
--- bottle.java
+++ bottle.java
@@ -1,4 +1,35 @@
+import java.util.Scanner;
+
 public class bottle {
     private float height;
     private int volume;
+    bottle(){}
+    bottle(float height ,int volume){
+        this.height=height;
+        this.volume=volume;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    public  void setDetails(){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Please enter a Height:");
+        float tempHeight=input.nextFloat();
+        this.height=tempHeight;
+        System.out.println("Please enter a Volume :");
+        int tempVolume=input.nextInt();
+        this.volume=tempVolume;
+    }
+    public float getHeight(float height){return this.height;}
+    public int getVolume(int volume){return this.volume;}
+    public void print(){
+        System.out.println("The Bottle Height is"+this.height+"cm");
+        System.out.println("The Bottle Volume is"+this.volume+"ml");
+    }
+
 }
